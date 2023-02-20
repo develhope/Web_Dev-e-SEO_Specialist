a17b2e1345faf73cf8e06cb71a19ba7d2d00b61d
Giorgio Grassini <giorgiograssini@gmail.com>
2023-02-20 09:41:33 +0100

    layout completed

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        "purple": "#845EC2",
+        "purple-light": "#D65DB1",
+        "red-light": "#FF6F91",
+        "orange-light": "#FF9671",
+        "yellow-light": "#FFC75F",
+        "yellow": "#F9F871",
+      },
+    },
+  },
+};
